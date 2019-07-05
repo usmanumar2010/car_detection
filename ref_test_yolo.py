9a99b7923095e3af6ef694004b63fcae543f2ae0
usman2019 <uumar@macrosoftinc.com>
2019-07-05 05:22:07 +0500

    Car Detecting

--- ref_test_yolo.py
+++ ref_test_yolo.py
@@ -16,10 +16,12 @@ from yad2k.models.keras_yolo import yolo_eval, yolo_head
 parser = argparse.ArgumentParser(
     description='Run a YOLO_v2 style detection model on test images..')
 parser.add_argument(
-    'model_path',
+    '-m',
+    '--model_path',
     help='path to h5 model file containing body'
     'of a YOLO_v2 model')
 parser.add_argument(
+
     '-a',
     '--anchors_path',
     help='path to anchors file, defaults to yolo_anchors.txt',
@@ -152,43 +154,49 @@ def _main(args):
             font='font/FiraMono-Medium.otf',
             size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
         thickness = (image.size[0] + image.size[1]) // 300
+        list_of_classes=list(out_classes)
+        if 2 in list_of_classes:
 
-        for i, c in reversed(list(enumerate(out_classes))):
-            predicted_class = class_names[c]
-            box = out_boxes[i]
-            score = out_scores[i]
+            for i, c in reversed(list(enumerate(out_classes))):
+                if class_names[c]=="car":
+                    predicted_class = class_names[c]
+                    box = out_boxes[i]
+                    score = out_scores[i]
 
-            label = '{} {:.2f}'.format(predicted_class, score)
+                    label = '{} {:.2f}'.format(predicted_class, score)
 
-            draw = ImageDraw.Draw(image)
-            label_size = draw.textsize(label, font)
+                    draw = ImageDraw.Draw(image)
+                    label_size = draw.textsize(label, font)
 
-            top, left, bottom, right = box
-            top = max(0, np.floor(top + 0.5).astype('int32'))
-            left = max(0, np.floor(left + 0.5).astype('int32'))
-            bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
-            right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
-            print(label, (left, top), (right, bottom))
+                    top, left, bottom, right = box
+                    top = max(0, np.floor(top + 0.5).astype('int32'))
+                    left = max(0, np.floor(left + 0.5).astype('int32'))
+                    bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+                    right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+                    print(label, (left, top), (right, bottom))
 
-            if top - label_size[1] >= 0:
-                text_origin = np.array([left, top - label_size[1]])
-            else:
-                text_origin = np.array([left, top + 1])
+                    if top - label_size[1] >= 0:
+                        text_origin = np.array([left, top - label_size[1]])
+                    else:
+                        text_origin = np.array([left, top + 1])
 
-            # My kingdom for a good redistributable image drawing library.
-            for i in range(thickness):
-                draw.rectangle(
-                    [left + i, top + i, right - i, bottom - i],
-                    outline=colors[c])
-            draw.rectangle(
-                [tuple(text_origin), tuple(text_origin + label_size)],
-                fill=colors[c])
-            draw.text(text_origin, label, fill=(0, 0, 0), font=font)
-            del draw
+                    # My kingdom for a good redistributable image drawing library.
+                    for i in range(thickness):
+                        draw.rectangle(
+                                [left + i, top + i, right - i, bottom - i],
+                                outline=colors[c])
+                    draw.rectangle(
+                            [tuple(text_origin), tuple(text_origin + label_size)],
+                            fill=colors[c])
+                    draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+                    del draw
 
-        image.save(os.path.join(output_path, image_file), quality=90)
+            image.save(os.path.join(output_path, image_file), quality=90)
+
     sess.close()
 
 
 if __name__ == '__main__':
+    print("usman")
+
     _main(parser.parse_args())
